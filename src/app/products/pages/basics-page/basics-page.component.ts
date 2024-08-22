8958dbe30f3115ffff96dc9a7aeaf058736d0f63
Kevin Carrasco <kepler401@gmail.com>
2024-08-22 15:57:36 -0600

    Prime flex agregado

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrl: './basics-page.component.css'
+})
+export class BasicsPageComponent {
+  public nameLower: string = 'kevin';
+  public nameUpper: string = 'KEVIN';
+  public fullName: string = 'keViN cARrascO';
+}
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 // import { MenuItem } from 'primeng/api';
 import {MenuItem} from 'primeng/api';
 
+
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
@@ -18,15 +19,18 @@ export class MenuComponent {
           items:[
             {
               label:'Textos y Fechas',
-              icon: 'pi pi-align-left'
+              icon: 'pi pi-align-left',
+              routerLink: ' '
             },
             {
               label:'Números',
-              icon: 'pi pi-dollar'
+              icon: 'pi pi-dollar',
+              routerLink: 'numbers'
             },
             {
               label:'No comunes',
-              icon: 'pi pi-globe'
+              icon: 'pi pi-globe',
+              routerLink: 'uncommon'
             },
           ]
         },
